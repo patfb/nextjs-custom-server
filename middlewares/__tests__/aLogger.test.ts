49433602bd527a753d1227e79330cf92edcd2fcc
patfb <snailmail>
2024-09-08 02:24:19 -0400

    trying to get jest linter to work

---
+++ middlewares/__tests__/aLogger.test.ts
@@ -0,0 +1,15 @@
+import { describe, expect, it } from "@jest/globals";
+import { aLogger } from "../aLogger";
+import { type NextRequest } from "next/server";
+
+describe("aLogger", () => {
+  const req = {
+    nextUrl: {
+      pathname: "/apples",
+    },
+  } as NextRequest;
+
+  it("should return undefined", () => {
+    // expect(aLogger(req)).toBeUndefined();
+  });
+});
