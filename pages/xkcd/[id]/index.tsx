9b2400da0c484e56ef1dc7b0aa8a045818c06f54
patfb <patfb>
2024-11-30 23:33:06 -0500

    cleaner xkcd render

--- pages/xkcd/[id]/index.tsx
+++ pages/xkcd/[id]/index.tsx
@@ -1,5 +1,5 @@
-import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
 import "@root/global.css";
+import type { GetServerSideProps, InferGetServerSidePropsType } from "next";
 
 interface Comic {
   alt: string;
@@ -16,23 +16,44 @@ interface Comic {
 }
 
 export const getServerSideProps = (async (context) => {
-  const id = context?.params?.id ?? 1;
+  const id = (context?.params?.id ?? "1") as string;
 
-  // Fetch data from external API
-  const res = await fetch(`https://xkcd.com/${id}/info.0.json`);
-  const comic: Comic = await res.json();
-  // Pass data to the page via props
-  return { props: { comic } };
-}) satisfies GetServerSideProps<{ comic: Comic }>;
+  try {
+    // Fetch data from external API
+    const res = await fetch(`https://xkcd.com/${id}/info.0.json`);
+    const comic: Comic = await res.json();
+    // Pass data to the page via props
+    return { props: { comic, id } };
+  } catch (error) {
+    return { props: { comic: null, id } };
+  }
+}) satisfies GetServerSideProps<{ comic: Comic | null; id: string }>;
 
 export default function xkcdIdPage({
   comic,
+  id,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const renderedComic = comic ? (
+    <>
+      <h2>{comic.title}</h2>
+
+      <p>
+        {comic.month}/{comic.day}/{comic.year}
+      </p>
+
+      <p>Transcript: {comic.transcript}</p>
+      <p>Alt: {comic.alt}</p>
+
+      <img src={comic.img} />
+    </>
+  ) : (
+    <p>could not find comic</p>
+  );
+
   return (
     <div>
-      <h1>xkcd #{comic.num}</h1>
-      <p>{JSON.stringify(comic)}</p>
-      <img src={comic.img} />
+      <h1>xkcd #{id}</h1>
+      {renderedComic}
     </div>
   );
 }
